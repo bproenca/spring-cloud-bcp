d32f13bc6e93584d1387669f5228c25732646972
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-03-28 15:09:28 -0300

    ribbon

--- currency-conversion-service/src/main/java/br/com/bcp/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/br/com/bcp/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,14 +1,23 @@
 package br.com.bcp.currencyconversionservice;
 
-//import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000") // with service discovery you don't need to hardcode URL
-//@FeignClient(name="currency-exchange-service")
+//  Plain Project:
+//  1- Add Feign (pom):  
+//    @FeignClient(name="currency-exchange-service", url="localhost:8000") // without service discovery (eureka) you have to hardcode URL
+//  2- Add Ribbon:  
+//    @FeignClient(name="currency-exchange-service")
+//    @RibbonClient(name="currency-exchange-service") [seens to be optional]
+//    applicatin.properties
+//      currency-exchange-service.ribbon.listOfServers=http://localhost:8000,http://localhost:8001
+
+//@FeignClient(name="currency-exchange-service", url="localhost:8000") // without service discovery (eureka) you have to hardcode URL
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 //@FeignClient(name="netflix-zuul-api-gateway-server")
-//@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     // Sem Zuul VVVVVV
