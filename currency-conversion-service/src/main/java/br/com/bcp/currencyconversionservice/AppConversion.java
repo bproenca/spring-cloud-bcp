6a43129b737889a716e32a181de2cf14f9536011
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-03-29 21:38:20 -0300

    spring cloud upgrade

--- currency-conversion-service/src/main/java/br/com/bcp/currencyconversionservice/AppConversion.java
+++ currency-conversion-service/src/main/java/br/com/bcp/currencyconversionservice/AppConversion.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-//import brave.sampler.Sampler;
-
 @SpringBootApplication
 @EnableFeignClients("br.com.bcp.currencyconversionservice")
 @EnableDiscoveryClient
@@ -20,10 +18,4 @@ public class AppConversion {
 		SpringApplication.run(AppConversion.class, args);
 	}
 
-	/*
-	@Bean
-	public Sampler defaultSampler() {
-		return Sampler.ALWAYS_SAMPLE;
-	}
-*/
 }
--- currency-exchange-service/src/main/java/br/com/bcp/currencyexchangeservice/AppExchange.java
+++ currency-exchange-service/src/main/java/br/com/bcp/currencyexchangeservice/AppExchange.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-//import brave.sampler.Sampler;
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AppExchange {
@@ -17,12 +15,5 @@ public class AppExchange {
 
 		SpringApplication.run(AppExchange.class, args);
 	}
-
-	/*
-	@Bean
-	public Sampler defaultSampler() {
-		return Sampler.ALWAYS_SAMPLE;
-	}
-	*/
 
 }
