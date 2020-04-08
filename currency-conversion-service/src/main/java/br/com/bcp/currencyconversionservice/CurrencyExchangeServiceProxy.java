99d9301c464b12374fe06af5666b99aadc37c5ca
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-04-07 22:59:01 -0300

    ccs ces dockerized

--- currency-conversion-service/src/main/java/br/com/bcp/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/br/com/bcp/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 // http://localhost:8765/{application-name}/{uri}/from/EUR/to/INR
 // http://localhost:8765/currency-exchange-service/currency-exchange/from/EUR/to/INR
 
-//@FeignClient(name="currency-exchange-service")
-@FeignClient(name="netflix-zuul-api-gateway-server")
-@RibbonClient(name="currency-exchange-service")
+@FeignClient(name = "currency-exchange-service", url = "${CURRENCY_EXCHANGE_URI}")
+//@FeignClient(name="netflix-zuul-api-gateway-server")
+//@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    //@GetMapping("/currency-exchange/from/{from}/to/{to}")
-    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    //@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
