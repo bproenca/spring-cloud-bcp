aafc95aab019a63d565ebda81601c1da352cb63d
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-05-03 15:30:59 -0300

    conversion local ok

--- currency-conversion-service/src/main/java/br/com/bcp/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/br/com/bcp/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.bcp.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("br.com.bcp.currencyconversionservice.resource")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
