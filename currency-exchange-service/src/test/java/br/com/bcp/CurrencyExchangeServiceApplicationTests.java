b9f47d2e5a87ca2547b6f1aaf7eac0516d6615bf
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-05-03 13:06:36 -0300

    https://start.spring.io/

---
+++ currency-exchange-service/src/test/java/br/com/bcp/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
